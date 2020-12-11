88af936472a3b7a9adeab37ef012804fac13962f
David Walzer <david.wz@gmx.net>
2020-12-11 11:15:07 +0100

    Block mining function added

---
+++ blockchain.js
@@ -0,0 +1,79 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+
+    calculateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp +
+            JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        const diffarr = Array(difficulty + 1).join("0");
+        var diff = difficulty + 1;
+        // diffarr.length = difficulty + 1;
+        // diffarr.join("0");
+
+        console.log(diffarr);
+        while (this.hash.substring(0, difficulty) !== diffarr) {
+            this.nonce++;
+
+            this.hash = this.calculateHash();
+        }
+
+        console.log('Block mined:' + this.hash);
+    }
+}
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 5;
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "03/12/2020", "Genesis block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty)
+        this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+let davidCoin = new Blockchain();
+davidCoin.addBlock(new Block(1, "03/12/2020", { amount: 10 }));
+console.log('mining block 1...')
+davidCoin.addBlock(new Block(2, "05/12/2020", { amount: 20 }));
+console.log('mining block2...')
+
+davidCoin.chain[1].data = { amount: 100 };
+console.log('Is blockchain valid?' + davidCoin.isChainValid());
+// console.log(JSON.stringify(davidCoin, null, 4))
